fd2323d7ab974ec6c79df165f40d691eab48fe34
Ivan Brajković <44271953+ivbrajkovic@users.noreply.github.com>
2019-11-02 19:27:24 +0100

    add GET check username

--- functions/routes/user.js
+++ functions/routes/user.js
@@ -11,12 +11,12 @@ const { firebase, db } = require('../admin');
  * @param res
  */
 exports.getUserDetailsById = (req, res) => {
+	// Validate id
+	if (!req.params) return res.status(400).json({ error: 'Invalid user id' });
+
 	// Get user id
 	const userId = req.params.id;
 	//require('../lib').logObj(req.params);
-
-	// Validate id
-	if (!req.params) return res.status(400).json({ error: 'Invalid user id' });
 
 	// Get user details from firebase
 	db.collection(`users`)
@@ -43,20 +43,21 @@ exports.getUserDetailsById = (req, res) => {
  * @param res
  */
 exports.checkUsername = (req, res) => {
-	// Get user id
-	const userId = req.params.username;
-	//require('../lib').logObj(req.params);
-
 	// Validate id
 	if (!req.params) return res.status(400).json({ error: 'Invalid user id' });
+
+	// Get user id
+	const username = req.params.username;
+	//require('../lib').logObj(req.params);
 
 	// Get user details from firebase
 	db.collection(`users`)
 		.doc(username)
 		.get()
-		.then(snapshot => {
-			if (snapshot.empty) return res.status(404).json({ error: 'User not found' });
-			else return res.status(404).json({ success: 'User found in database' });
+		.then(doc => {
+			// require('../lib').logObj(doc);
+			if (!doc.exists) return res.status(404).json({ success: false, error: 'User not found' });
+			else return res.status(404).json({ success: true, message: 'User found in database' });
 		})
 		.catch(err => {
 			console.log(`Error getting documents, error: ${err}`);
